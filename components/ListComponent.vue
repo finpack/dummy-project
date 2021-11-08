ee1d574fc6f37f0269a924f702da1c83ccebfacc
kstraszewski <koonradstraszewski@gmail.com>
2021-11-08 23:05:58 +0100

    add todo element

--- components/ListComponent.vue
+++ components/ListComponent.vue
@@ -1,10 +1,21 @@
-<script lang="ts">
+<template>
+  <div>
+    {{todo}}
+  </div>
+</template>
+<script lang="ts" setup>
+import {PropType} from '@vue/runtime-core';
+import {Todo} from '~/models/Todo';
+
+export default defineComponent({
+  props: {
+    todo: {
+      required: true,
+      type: Object as PropType<Todo>
+    }
+  }
+})
 
 </script>
-
-<template>
-
-</template>
-
 <style scoped lang="scss">
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,24 @@
 <script setup lang="ts">
+import {Todo} from '~/models/Todo';
+import {Ref} from '@vue/reactivity';
+import ListComponent from '~/components/ListComponent.vue';
 
+const todos: Ref<Todo[]> = ref([]);
+
+onMounted(() => {
+  todos.value = [];
+})
 </script>
 <template>
   <div class="container mx-auto">
     <div class="heading text-center">
       ToDo lista
     </div>
-    <div class="">
-
+    <div>
+      <div v-for="todo in todos">
+        <ListComponent :todo="todo"></ListComponent>
+      </div>
+      {{todos}}
     </div>
   </div>
 </template>
