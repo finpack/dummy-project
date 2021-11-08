afab16fb61790fd56376094ed1ef506a6508948c
kstraszewski <koonradstraszewski@gmail.com>
2021-11-08 22:03:00 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <div class="container mx-auto">
+    <div class="heading text-center">
+      ToDo lista
+    </div>
+    <div class="">
+
+    </div>
+  </div>
+</template>
